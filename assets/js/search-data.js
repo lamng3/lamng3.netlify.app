de64e6ab3fde7dd20c01ddfabdb974f4c85a0588
lamng3 <nguyentamhoanglam@gmail.com>
2025-07-19 18:32:26 +0000

    Deploying to gh-pages from @ lamng3/lamng3.netlify.app@8edaed339adc046863440b2736ad4f2c96096c69 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -56,6 +56,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/ltn18", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=ws80Q0IAAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
